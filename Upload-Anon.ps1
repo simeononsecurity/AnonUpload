a5f2c41a05ec225569136320eb420c64c0e1381f
fT5SgrjXeV8khTNVgQmD <4913771+simeononsecurity@users.noreply.github.com>
2020-11-10 18:21:19 -0600

    Add Metadata

--- Upload-Anon.ps1
+++ Upload-Anon.ps1
@@ -1,3 +1,26 @@
+<#PSScriptInfo
+
+.VERSION 1.0
+
+.GUID cabe1358-d9ac-43fc-9b8e-4917152718a1
+
+.AUTHOR simeon@simeononsecurity.ch
+
+.COMPANYNAME SimeonOnSecurity
+
+.COPYRIGHT 2020 SimeonOnSecurity. All rights reserved.
+
+.TAGS AnonFiles Upload File Share FileShare
+
+.PROJECTURI https://github.com/simeononsecurity/Upload-Anon_PS
+
+.DESCRIPTION "Upload to AnonFiles.com easily Ex. 'Upload-Anon -File 'C:\temp\test.txt'"
+
+.RELEASENOTES
+Init
+
+#>
+
 function Upload-Anon {
 #Requires -Version 6.0
 param(
