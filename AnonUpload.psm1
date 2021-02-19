db75110008d468753ab4f0d8ff118602107bfd56
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2021-02-19 14:55:19 -0600

    Change to AnonUpload

--- AnonUpload.psm1
+++ AnonUpload.psm1
@@ -12,22 +12,22 @@
 
 .TAGS AnonFiles Upload File Share FileShare
 
-.PROJECTURI https://github.com/simeononsecurity/Upload-Anon_PS
+.PROJECTURI https://github.com/simeononsecurity/AnonUpload_PS
 
-.DESCRIPTION "Upload to AnonFiles.com easily Ex. 'Upload-Anon -File 'C:\temp\test.txt'"
+.DESCRIPTION "Upload to AnonFiles.com easily Ex. 'AnonUpload -File 'C:\temp\test.txt'"
 
 .RELEASENOTES
 Init
 
 #>
 
-function Upload-Anon {
+function AnonUpload {
     #Requires -Version 6.0
     param(
         [string]$File 
     )
     If (!$File) {
-        Write-Host "Please provide the a file. Ex: Upload-Anon -File 'C:\temp\test.txt'"
+        Write-Host "Please provide the a file. Ex: AnonUpload -File 'C:\temp\test.txt'"
     }
     Else {
         Write-Host "Please wait wile the file is uploaded"
